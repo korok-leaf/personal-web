6897a52b32d8170d0151c626edae736293d5339c
Alex Qin <pdsalex2018@gmail.com>
2025-01-04 20:16:18 -0500

    small change n2

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -10,7 +10,7 @@ const Main = () => {
 
     return (
         <div className="relative w-screen h-screen bg-black overflow-scroll min-w-96">
-            <div className="absolute h-2/3 w-screen">
+            <div className="absolute h-2/3 w-screen overflow-hidden">
                 <BackgroundPic/>  
             </div>
             <div className="relative p-10">
